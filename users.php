f5283d26f3975e496f80ea443359862389a852c5
Evandorizki36 <evandorizki36@gmail.com>
2026-06-10 19:15:19 +0700

    commit pertama

---
+++ users.php
@@ -0,0 +1,64 @@
+<?php
+
+class Users
+{
+    private $conn;
+    private $table = "users";
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function create($Username, $email, $Asal, $password)
+    {
+        $sql = "INSERT INTO users
+                (Username, email, Asal, password)
+                VALUES (?, ?, ?, ?)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bind_param(
+            "ssss",
+            $Username,
+            $email,
+            $Asal,
+            $password
+        );
+
+        return $stmt->execute();
+    }
+
+    public function login($username, $password)
+    {
+        $sql = "SELECT * FROM users
+                WHERE Username = ?
+                AND password = ?";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bind_param(
+            "ss",
+            $username,
+            $password
+        );
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        return $result->fetch_assoc();
+    }
+    public function getAllUsers()
+    {
+        $sql = "SELECT * FROM $this->table";
+        $result = $this->conn->query($sql);
+
+        if($result->num_rows > 0){
+            return $result;
+        } else {
+            return null;
+        }
+    }
+}
+?>
